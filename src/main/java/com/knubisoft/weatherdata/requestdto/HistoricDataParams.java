c3b996bdee1e2316d22147a2031ed6403d56ddbd
julivoloshyn <yuliavoloshina02@gmail.com>
2022-10-08 19:22:45 +0300

    request parameters

--- src/main/java/com/knubisoft/weatherdata/requestdto/HistoricDataParams.java
+++ src/main/java/com/knubisoft/weatherdata/requestdto/HistoricDataParams.java
@@ -1,27 +1,28 @@
 package com.knubisoft.weatherdata.requestdto;
 
-import com.sun.istack.NotNull;
+import com.knubisoft.weatherdata.requestdto.validation.annotation.DateTimeFormat;
+import com.knubisoft.weatherdata.requestdto.validation.annotation.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
+@Validated
 public class HistoricDataParams {
 
-    @NotNull
+    @DateTimeFormat(message = "Incorrect date time format.")
+    @NotEmpty(message = "Date can not be empty.")
     private String startDateTime;
 
-    @NotNull
-    private String aggregateHours;
-
-    @NotNull
+    @NotEmpty(message = "Location can not be empty.")
     private String location;
 
-    @NotNull
+    @NotEmpty(message = "Date can not be empty.")
+    @DateTimeFormat(message = "Incorrect date time format.")
     private String endDateTime;
-
-    @NotNull
-    private String unitGroup;
 }
