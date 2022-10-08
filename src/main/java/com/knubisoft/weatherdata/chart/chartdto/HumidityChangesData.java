d3a5836d95ddbdfe08e45f259ccce0fb7eff5a64
julivoloshyn <yuliavoloshina02@gmail.com>
2022-10-08 19:23:02 +0300

    dto for charts

--- src/main/java/com/knubisoft/weatherdata/chart/chartdto/HumidityChangesData.java
+++ src/main/java/com/knubisoft/weatherdata/chart/chartdto/HumidityChangesData.java
@@ -1,8 +1,10 @@
 package com.knubisoft.weatherdata.chart.chartdto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class HumidityChangesData {
     private double humidity;
 }
--- src/main/java/com/knubisoft/weatherdata/chart/chartdto/TemperatureChangesData.java
+++ src/main/java/com/knubisoft/weatherdata/chart/chartdto/TemperatureChangesData.java
@@ -1,8 +1,10 @@
 package com.knubisoft.weatherdata.chart.chartdto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class TemperatureChangesData {
     private double maxt;
     private double mint;
