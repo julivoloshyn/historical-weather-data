c3d97e2415ebbdd68e166c21e9df309457bf369d
julivoloshyn <yuliavoloshina02@gmail.com>
2022-10-08 19:16:24 +0300

    clean up

--- src/main/java/com/knubisoft/weatherdata/chart/chageschart/ChangesChart.java
+++
@@ -1,8 +0,0 @@
-package com.knubisoft.weatherdata.chart.chageschart;
-
-import java.util.List;
-
-public interface ChangesChart {
-
-    <T> void createChart(List<T> data, String startDateTime, String endDateTime);
-}
