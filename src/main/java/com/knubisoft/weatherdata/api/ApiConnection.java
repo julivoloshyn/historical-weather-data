f9e2bcfae3843173a26aaf5ad96d77e1a5e125f6
julivoloshyn <yuliavoloshina02@gmail.com>
2022-10-08 19:15:07 +0300

    api connection

--- src/main/java/com/knubisoft/weatherdata/api/ApiConnection.java
+++ src/main/java/com/knubisoft/weatherdata/api/ApiConnection.java
@@ -9,21 +9,14 @@ import java.net.http.HttpResponse;
 
 public class ApiConnection {
 
+    /**
+     * Creates connection with api.
+     *
+     * @param uri Request for API.
+     * @return response.
+     */
     @SneakyThrows
-    public String forecast() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0"))
-                .header("X-RapidAPI-Key", "e383515f77mshb40c1345863fa6ep18c8fdjsndf718dcb823e")
-                .header("X-RapidAPI-Host", "visual-crossing-weather.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
-    }
-
-    @SneakyThrows
-    public String historicData(String uri) {
+    public String connectHistoricData(String uri) {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("X-RapidAPI-Key", "e383515f77mshb40c1345863fa6ep18c8fdjsndf718dcb823e")
